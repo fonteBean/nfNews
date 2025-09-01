6edcd02e3258d8bd1765599244a0b3d71136313c
fonteBean <n9ckbean@gmail.com>
2025-09-01 17:43:32 -0300

    Back-End: first commit

---
+++ src/knexfile.js
@@ -0,0 +1,42 @@
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+
+require('dotenv').config();
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      host: '127.0.0.1',
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    },
+    migrations: {
+        directory: './db/migrations',
+      },
+    seeds: {
+        directory: './db/seeds',
+      },
+  },
+  ci: {
+    client: 'pg',
+    connection: {
+      host: 'postgres', 
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    },
+    migrations: {
+      directory: './db/migrations',
+    },
+    seeds: {
+      directory: './db/seeds',
+    },
+  }
+
+};
